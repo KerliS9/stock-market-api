0a60e4e7a47ef7d62c4b4b4ad07f28043fbdf837
KerliS9 <kerlischroeder9@gmail.com>
2022-07-18 20:41:43 -0300

    fix - add collumn for asset sold

--- StockMarketDB.sql
+++ StockMarketDB.sql
@@ -49,13 +49,20 @@ CREATE TABLE StockMarketDB.Customer_Investments (
   id int AUTO_INCREMENT NOT NULL,
   customer_id int NOT NULL,
   asset_id int NOT NULL,
-  amount_asset int NOT NULL,
+  amount_asset_take int,
+  amount_asset_sell int,
   date DATETIME DEFAULT CURRENT_TIMESTAMP, 
   PRIMARY KEY (id),
   FOREIGN KEY (customer_id) REFERENCES StockMarketDB.Customer (id) ON DELETE CASCADE,
   FOREIGN KEY (asset_id) REFERENCES StockMarketDB.Market_Assets (id) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
+CREATE TABLE StockMarketDB.Customer_Custody (
+	customer_id int NOT NULL,
+    asset_id int NOT NULL,
+	amount_asset int NOT NULL,
+    PRIMARY KEY (customer_id, asset_id)
+)ENGINE=InnoDB;
 SET SQL_SAFE_UPDATES = 0;
 
 INSERT INTO
@@ -104,15 +111,15 @@ VALUES
   (4, "PETROLEO BRASILEIRO S.A. PETROBRAS", "Petróleo");
   
 INSERT INTO
-  StockMarketDB.Customer_Investments (customer_id, asset_id, amount_asset, date)
+  StockMarketDB.Customer_Investments (customer_id, asset_id, amount_asset_take, amount_asset_sell, date)
 VALUES
-  (1, 1, 100, NOW()),
-  (1, 2, 200, NOW()),
-  (1, 3, 500, NOW()),
-  (1, 4, 400, NOW()),
-  (2, 2, 150, NOW()),
-  (2, 3, 100, NOW()),
-  (2, 4, 100, NOW()),
-  (3, 1, 500, NOW()),
-  (3, 3, 500, NOW()),
-  (3, 4, 400, NOW());
+  (1, 1, 100, 10, NOW()),
+  (1, 2, 200, 10, NOW()),
+  (1, 3, 500, 10, NOW()),
+  (1, 4, 400, 10, NOW()),
+  (2, 2, 150, 10, NOW()),
+  (2, 3, 100, 10, NOW()),
+  (2, 4, 100, 10, NOW()),
+  (3, 1, 500, 10, NOW()),
+  (3, 3, 500, 10, NOW()),
+  (3, 4, 400, 10, NOW());
