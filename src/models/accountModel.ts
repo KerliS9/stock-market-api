3f6731309d1f6285ab02382c9ae3d6cb9cadc207
KerliS9 <kerlischroeder9@gmail.com>
2022-07-18 11:49:11 -0300

    fix - query to get all customer

--- src/models/accountModel.ts
+++ src/models/accountModel.ts
@@ -2,7 +2,13 @@ import Connection from './connection';
 
 export default {
   getAll: async () => {
-    const query = 'SELECT * FROM Customer;';
+    const query = 'SELECT full_name AS fullName, investor_profile AS investorProfile, account_balance AS accountBalance FROM Customer;';
+    const [result] = await Connection.execute(query);
+    return result;
+  },
+
+  getCustomerById: async () => {
+    const query = 'SELECT full_name AS fullName, investor_profile AS investorProfile, account_balance AS accountBalance FROM Customer;';
     const [result] = await Connection.execute(query);
     return result;
   },
