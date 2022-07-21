f65dad4222e59c1ce4e0af23088c172e17424caf
KerliS9 <kerlischroeder9@gmail.com>
2022-07-21 14:15:54 -0300

    fix - tests getall customers on level service

--- test/unit/services/accountService.spec.ts
+++ test/unit/services/accountService.spec.ts
@@ -3,23 +3,23 @@ import AccountModel from '../../../src/models/accountModel';
 import AccountService from '../../../src/services/accountService';
 import { customers, customerById, customerByIdSaved, AccountStatement } from '../../__mocks__/account';
 
-/* describe('Check Asset Service GET: getAllAssets assets from database', () => {    
+describe('Check Asset Service GET: getAllCustomers assets from database', () => {    
     it('should return an array of objects that contains the keys customerId, fullName, investorProfile', async () => {
     jest.spyOn(AccountModel, 'getAllCustomers').mockResolvedValue(customers);
     const response = await AccountService.getAllCustomers();
     expect(response).toEqual(expect.arrayContaining(customers));
-    expect(response[0]).toHaveProperty('id');
-    expect(response[0]).toHaveProperty('asset');
-    expect(response[0]).toHaveProperty('price');
-    expect(response[0]).toHaveProperty('sector');
-    expect(response[0]).toHaveProperty('company');
+    expect(response[0]).toHaveProperty('customerId');
+    expect(response[0]).toHaveProperty('fullName');
+    expect(response[0]).toHaveProperty('investorProfile');
   });
-}); */
+});
 
-describe('Check Account Service GET: getCustomerById from database', () => {
+/* describe('Check Account Service GET: getCustomerById from database', () => {
   it('should return an object that contains the keys customerId, fullName, investorProfile, accountBalance', async () => {
     console.log('antes', await AccountModel.getCustomerById(1));
-    const mockFn = jest.fn(AccountModel.getCustomerById);
+    // const mockFn = jest.fn(AccountModel.getCustomerById);
+    // mockFn.mockImplementation(() => Promise.resolve(customerById))
+    jest.spyOn(AccountModel, 'getCustomerById').mockResolvedValue(customerByIdSaved)
     jest.fn(AccountModel.getCustomerAccountBalance);
     // jest.spyOn(AccountModel, 'getCustomerById').mockResolvedValue(customerByIdSaved as any);
     jest.spyOn(AccountModel, 'getCustomerAccountBalance').mockResolvedValue(AccountStatement as any);
@@ -30,4 +30,4 @@ describe('Check Account Service GET: getCustomerById from database', () => {
     expect(response).toHaveProperty('investorProfile');
     expect(response).toHaveProperty('accountBalance');
   });
-});
+}); */
