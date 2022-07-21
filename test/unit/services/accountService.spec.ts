bd1b19c97531fed5d6cbff959e2c036806356312
KerliS9 <kerlischroeder9@gmail.com>
2022-07-21 20:06:12 -0300

    feat - tests service leve function getAccountStatementByCustomerId

--- test/unit/services/accountService.spec.ts
+++ test/unit/services/accountService.spec.ts
@@ -1,9 +1,9 @@
 import AccountModel from '../../../src/models/accountModel';
 import AccountService from '../../../src/services/accountService';
 import { customers, accountBalance, customerById,
-  ResultSetHeaderMock } from '../../__mocks__/account';
+  ResultSetHeaderMock, customerByIdAccountStatement } from '../../__mocks__/account';
 
-describe('Check Asset Service GET: getAllCustomers assets from database', () => {    
+describe('Check Account Service GET: getAllCustomers assets from database', () => {    
     it('should return an array of objects that contains the keys customerId, fullName, investorProfile', async () => {
     jest.spyOn(AccountModel, 'getAllCustomers').mockResolvedValue(customers);
     const response = await AccountService.getAllCustomers();
@@ -21,7 +21,6 @@ describe('Check Account Service GET: getCustomerById from database', () => {
     jest.spyOn(AccountModel, 'updateAccountBalanceByCustomerId').mockResolvedValue(ResultSetHeaderMock as any);
     jest.spyOn(AccountModel, 'getCustomerById').mockResolvedValue(customerById);
     const [response] = await AccountService.getCustomerById(1);
-    // console.log('getCustomerById', [response]);
     expect(response).toHaveProperty('customerId');
     expect(response).toHaveProperty('fullName');
     expect(response).toHaveProperty('investorProfile');
@@ -30,8 +29,18 @@ describe('Check Account Service GET: getCustomerById from database', () => {
   it('when don\t exists this customerId on database', async () => {
     jest.spyOn(AccountModel, 'getCustomerById').mockResolvedValue([]);
     const [response] = await AccountService.getCustomerById(10);
-    console.log('getCustomerById', response);
     expect(response).toHaveProperty('message');
     expect(response.message).toBe('Sorry, this customer still doesn\t have an account with us');
   })
+});
+
+describe('Check Account Service GET: getAccountStatementByCustomerId from database', () => {    
+  it('should return an array of objects that contains the keys date, accountInput, accountOutput', async () => {
+    jest.spyOn(AccountModel, 'getAccountStatementByCustomerId').mockResolvedValue(customerByIdAccountStatement);
+    const response = await AccountService.getAccountStatementByCustomerId(1);
+    expect(response).toEqual(expect.arrayContaining(customerByIdAccountStatement));
+    expect(response[0]).toHaveProperty('date');
+    expect(response[0]).toHaveProperty('accountInput');
+    expect(response[0]).toHaveProperty('accountOutput');
+  });
 });
