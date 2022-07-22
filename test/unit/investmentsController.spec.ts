617270617dbec93c6f2ceb419f90a0357ca9e8a5
KerliS9 <kerlischroeder9@gmail.com>
2022-07-22 10:51:14 -0300

    feat - test controller level for buy and sell asset

---
+++ test/unit/investmentsController.spec.ts
@@ -0,0 +1,37 @@
+import InvestmentsController from '../../src/controllers/investmentsController';
+import { buyAsset, sellAsset } from '../__mocks__/investments';
+import { getMockReq, getMockRes } from '@jest-mock/express'
+
+describe('Check Investments Controller POST: buy asset for customer that requested', () => {
+  it('should return an object', async () => {
+    const req = getMockReq({
+      body: {
+        customerId: 1,
+        assetId: 2,
+        quantity: 5,
+      } })
+
+    const { res } = getMockRes();
+
+    await InvestmentsController.buyAsset(req, res);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledWith(expect.objectContaining(buyAsset));
+    });
+});
+
+describe('Check Investments Controller POST: buy asset for customer that requested', () => {
+  it('should return an object', async () => {
+    const req = getMockReq({
+      body: {
+        customerId: 1,
+        assetId: 2,
+        quantity: 5,
+      } })
+
+    const { res } = getMockRes();
+
+    await InvestmentsController.sellAsset(req, res);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledWith(expect.objectContaining(sellAsset));
+    });
+});
