233e3d671a94059caa1a25b6ff65862dea1620e0
KerliS9 <kerlischroeder9@gmail.com>
2022-07-23 16:29:04 -0300

    docs - building aplication

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
 };
